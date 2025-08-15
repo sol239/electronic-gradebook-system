1f3cc5b4e9c01d9b1abe78f67982d83172ff0537
David Válek <david.valek17@gmail.com>
2025-08-15 09:02:17 +0200

    Rename file and add sequence creation

---
+++ create_schema.sql
@@ -0,0 +1,22 @@
+-- Create tables
+@tables/tbl_parent.sql
+@tables/tbl_teacher.sql
+@tables/tbl_subject.sql
+@tables/tbl_class.sql
+@tables/tbl_student.sql
+@tables/tbl_classroom.sql
+@tables/tbl_lecture.sql
+@tables/tbl_grade.sql
+@tables/tbl_student_subject.sql
+@tables/tbl_subject_teacher.sql
+@tables/tbl_student_parent.sql
+
+-- Create sequences
+@sequences/seq_parent_id.sql
+@sequences/seq_teacher_id.sql
+@sequences/seq_subject_id.sql
+@sequences/seq_class_id.sql
+@sequences/seq_student_id.sql
+@sequences/seq_classroom_id.sql
+@sequences/seq_lecture_id.sql
+@sequences/seq_grade_id.sql
