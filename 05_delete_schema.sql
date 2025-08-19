34d62a0d9d1c3f2c43f5b3c2170012a1809ee82b
David Válek <david.valek17@gmail.com>
2025-08-20 00:50:02 +0200

    Delete indexes

--- 05_delete_schema.sql
+++ 05_delete_schema.sql
@@ -5,6 +5,50 @@
     Description: Script for deleting the schema.
 */
 
+-- Drop all indexes
+drop index idx_student_person_id;
+drop index idx_student_class_id;
+drop index idx_teacher_person_id;
+drop index idx_class_teacher_id;
+drop index idx_parent_person_id;
+drop index idx_grade_group_subject_id;
+drop index idx_grade_group_teacher_id;
+drop index idx_lecture_subject_id;
+drop index idx_lecture_classroom_id;
+drop index idx_class_group_class_id;
+drop index idx_lecture_student_lecture_id;
+drop index idx_lecture_student_student_id;
+drop index idx_lecture_teacher_lecture_id;
+drop index idx_lecture_teacher_teacher_id;
+drop index idx_classroom_lecture_classroom_id;
+drop index idx_classroom_lecture_lecture_id;
+drop index idx_subject_teacher_subject_id;
+drop index idx_subject_teacher_teacher_id;
+drop index idx_student_subject_student_id;
+drop index idx_student_subject_subject_id;
+drop index idx_student_parent_student_id;
+drop index idx_student_parent_parent_id;
+drop index idx_grade_group_student_grade_group_id;
+drop index idx_grade_group_student_student_id;
+drop index idx_class_group_student_class_group_id;
+drop index idx_class_group_student_student_id;
+drop index idx_grade_group_grade_group_date;
+drop index idx_lecture_start_time;
+drop index idx_lecture_end_time;
+drop index idx_grade_group_student_grade_date;
+drop index idx_grade_group_student_student_date;
+drop index idx_lecture_subject_time;
+drop index idx_grade_group_subject_date;
+drop index idx_student_class;
+drop index idx_subject_teacher_teacher_subject;
+drop index idx_grade_group_teacher_date;
+drop index idx_lecture_classroom_time;
+drop index idx_person_name;
+drop index idx_class_name;
+drop index idx_classroom_name;
+drop index idx_grade_group_student_grade;
+drop index idx_grade_group_student_message;
+
 -- Drop all triggers
 drop trigger trg_grade_group_biu;
 drop trigger trg_grade_group_student_biu;
