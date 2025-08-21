015d65f68f9ca5e1e1b9cc939b21fdeaef848147
David Válek <david.valek17@gmail.com>
2025-08-21 04:46:07 +0200

    Update code

--- create_commands/create_views.sql
+++ create_commands/create_views.sql
@@ -5,8 +5,12 @@
    Created: 2025-08-15
 */
 
-@views/vw_class.sql
 @views/vw_class_students.sql
-@views/vw_teacher_subjects.sql
-@views/vw_students_parents.sql
+@views/vw_class.sql
+@views/vw_student_grades_details.sql
 @views/vw_student_grades_summary.sql
+@views/vw_student_lectures.sql
+@views/vw_students_parents.sql
+@views/vw_teacher_lectures.sql
+@views/vw_teacher_subjects.sql
+@views/vw_student_teacher.sql
